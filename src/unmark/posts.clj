bf84aae5b54bf4ab22837f6f6bde78ad7c90c955
John Jacobsen <john@mail.npxdesigns.com>
2015-11-18 21:56:53 -0600

    Add LP post.

--- src/unmark/posts.clj
+++ src/unmark/posts.clj
@@ -663,7 +663,6 @@ as directly as possible."]
        next month"] " which, who knows? … might help someday."]))))
 
 
-
 (defpost "Marginalia Hacks"
   "marginalia-hacks"
   {:created "2014-08-03"}
@@ -891,3 +890,142 @@ other shivering, clicking and squinting Polies) were worth it. As an
 added benefit, I am half-way shifted over to the day schedule now, which
 will make returning to NZ more restful.
 ")))
+
+
+
+(defpost
+  "Communicating With Humans"
+  "communicating-with-humans"
+  {:created "2014-08-02"}
+  (postbody
+   (section "Communicating With Humans"
+     (epigraph  "If nobody but me likes it, let it die."
+                [:a {:href "http://www.informit.com/articles/article.aspx?p=1193856"} "Knuth"])
+     (img "marg-printed")
+     [:span
+      "This is the fifth post in a "
+      [:a {:href "/clojure/2014/07/03/an-advanced-clojure-workflow/"} "series"]
+      " about my Clojure workflow."]
+     "When you encounter a new codebase, what best allows you to
+     quickly understand it so that you can make effective changes to
+     it?"
+     "I switched jobs about six months ago. There was intense
+     information transfer both while leaving my old projects behind,
+     and while getting up to speed with new ones. I printed out a lot
+     of code and read it front-to-back, quickly at first, and then
+     carefully. I found this a surprisingly effective way to review
+     and learn, compared to my usual way of navigating code on disk
+     and in an editor solely on an as-needed basis. "
+     "If this (admittedly old-school) way of understanding a program
+     works well, how much better might it work if there was enough
+     prose interspersed in amongst the code to explain anything
+     non-obvious, and if the order of the text was presented in such
+     a way as to aid understanding? "
+     "What is the target audience of computer programs, anyways? It is
+     clearly the machines, which have to carry out our insanely
+     specific instructions... but, equally clearly, it is also the
+     humans who have to read, understand, maintain, fix, and extend
+     those programs. It astonishes me now how little attention is
+     paid to this basic fact. "
+     ["In addition to communicating, we also have to "
+      [:em "think carefully"] " about our work. While not every
+      programming problem is so difficult as to merit "
+      [:a {:href
+           "https://www.youtube.com/watch?v=f84n5oFoZBc"}
+       " a year's worth
+      of contemplation"]
+      ", any software system of significant size requires continual
+      care, attention, and occasional hard thinking in order to keep
+      complexity under control. The best way I know to think clearly
+      about a problem is to write about it &#x2013; the harder the
+      problem, the more careful and comprehensive the required
+      writing. "]
+     "Writing aids thinking, because it is slower than
+     thought... because you can replay thoughts over and over, iterate
+     upon and refine them. Because writing is explaining, and because
+     explaining something is the best way I know to learn and
+     understand it. "
+     [[:a {:href "http://en.wikipedia.org/wiki/Literate_programming"} "Literate Programming"]
+      " (LP) was invented by Donald Knuth in the 1980s as a way to
+       address some of these concerns. LP has hardcore enthusiasts
+       scattered about, but apparently not much traction in the
+       mainstream. As I have gotten more experience working with
+       complex codebases, and more engaged with the craft or
+       programming, I have become increasingly interested in LP as a
+       way to write good programs. Knuth takes it further, considering
+       the possibility that programs are, or could be, " [:em "works
+       of literature"] ". "]
+     "Knuth's innovation was both in realizing these possibilities and
+     in implementing the first system for LP, called WEB. WEB takes a
+     document containing a mix of prose and code and both typesets it
+     in a readable (beautiful, even) form for humans, and also orders
+     and assembles the program for a compiler to consume. "
+     ["Descendents and variants of WEB can be found in use
+      today. My favorite for Clojure is currently "
+      [:a {:href
+           "https://github.com/gdeer81/marginalia"} "Marginalia"] ",
+      originally by Michael Fogus and currently maintained by Gary
+      Deer. "]
+     [[:a
+       {:href "http://web.archive.org/web/20090102151452/http://www.perl.com/pub/a/tchrist/litprog.html"}
+       "Purists of LP will disagree"]
+      " that systems like Marginalia, which do not support reordering
+       and reassembly of source code, are \"true\" Literate
+       Programming tools; and, in fact, there is a caveat on the
+       Marginalia docs to that effect... but what Marginalia provides
+       is good enough for me: "]
+     [:ol
+      [:li "Placement of comments and docstrings adjacent to the
+      code in question; "]
+      [:li "Beautiful formatting of same; "]
+      [:li "Support for Markdown/HTML and attachment of JavaScript
+      and/or CSS files; therefore, for images, mathematics (via
+      MathJax) and graphing (see next blog post). "]]
+     "The result of these capabilities is a lightweight tool which
+       lets me take an existing Clojure project and, with very little
+       extra effort, generate a Web-based or printed/PDF artifact
+       which I can sit down with, learn from, and enjoy contemplating."
+     (subsubsection "Marginalia in Action:"
+       (img "marg-screenshot")
+       (img "emacs-screenshot"))
+     (subsection "The Notebook Pattern"
+       "I often start writing by making simple statements or questions: "
+       [:ol
+        [:li "I want to be able to do \\(X\\).... "]
+        [:li "I don't understand \\(Y\\).... "]
+        [:li "If we had feature \\(P\\), then \\(Q\\) would be easy.... "]
+        [:li "How long would it take to compute \\(Z\\)? "]]
+       ["Sentences like these are like snippets of code in the
+         REPL: things to evaluate and experiment with. Often these
+         statements are attached to bits of code -- experimental
+         expressions, and their evaluated results. They are the
+         building blocks of further ideas, programs, and chains of
+         thought. In "
+        [:a {:href "/clojure/2014/08/03/marginalia-hacks/"} "my next post"]
+        ", I'll talk about using Marginalia to make small notebooks
+         where I collect written thoughts, code, expression, even
+         graphs and plots while working on a problem. This workflow
+         involves some Marginalia hacks you may not see elsewhere. "]
+       "Meanwhile, here are some quotes about LP:"
+       "\"Instead of writing code containing documentation, the
+       literate programmer writes documentation containing
+       code.... The effect of this simple shift of emphasis can be so
+       profound as to change one's whole approach to programming.\"
+       --Ross Williams, FunnelWeb Tutorial Manual, p.4. "
+       "\"Knuth's insight is to focus on the program as a message
+       from its author to its readers.\" --Jon Bently,
+       \"Programming Pearls,\" Communications of the ACM, 1986."
+       ["\"... Literate programming is certainly the most
+         important thing that came out of the TeX project. Not only
+         has it enabled me to write and maintain programs faster and
+         more reliably than ever before, and been one of my greatest
+         sources of joy since the 1980s--it has actually been
+         indispensable at times. Some of my major programs, such as
+         the MMIX meta-simulator, could not have been written with any
+         other methodology that I've ever heard of. The complexity was
+         simply too daunting for my limited brain to handle; without
+         literate programming, the whole enterprise would have flopped
+         miserably.\" --Donald Knuth, "
+        [:a {:href
+             "http://www.informit.com/articles/article.aspx?p=1193856&rll=1"}
+         "interview"] ", 2008."]))))
