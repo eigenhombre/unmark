5148692abeb4d05e001cb727027b225dc929d44d
John Jacobsen <john@mail.npxdesigns.com>
2015-12-01 21:17:26 -0600

    Add analytics.

--- src/unmark/impl.clj
+++ src/unmark/impl.clj
@@ -53,15 +53,28 @@
 
 
 (defn page-footer []
-  [:div
-   [:p
-    [:a {:href "about-me.html"} "about"] "|"
-    [:a {:href "content.html"} "all posts"]]
-   [:p "© " (year) " " [:a {:href "about.html"} "John Jacobsen"]
-    ". Created with "
-    [:a {:href "https://github.com/eigenhombre/unmark"} "unmark"]
-    ".  CSS by "
-    [:a {:href "https://edwardtufte.github.io/tufte-css/"} "Tufte-CSS"] "."]])
+  [[:div
+    [:p
+     [:a {:href "about-me.html"} "about"] "|"
+     [:a {:href "content.html"} "all posts"]]
+    [:p "© " (year) " " [:a {:href "about-me.html"} "John Jacobsen"]
+     ". Created with "
+     [:a {:href "https://github.com/eigenhombre/unmark"} "unmark"]
+     ".  CSS by "
+     [:a {:href "https://edwardtufte.github.io/tufte-css/"} "Tufte-CSS"] "."]]
+   [:script {:type "text/javascript"}
+    "var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-40279882-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+      var ga = document.createElement('script');
+      ga.type = 'text/javascript';
+      ga.async = true;
+      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www')
+               + '.google-analytics.com/ga.js';
+      var s = document.getElementsByTagName('script')[0]; 
+      s.parentNode.insertBefore(ga, s);
+})();"]])
 
 
 (defn replace-symbols [txt]
@@ -174,8 +187,8 @@
      ~@(if show-author?
          [[:p {:class "subtitle"} "John Jacobsen"]]
          [])
-     ~@(walk-replace body)]
-   (walk-replace (page-footer))))
+     ~@(walk-replace body)
+     ~@(walk-replace (page-footer))]))
 
 
 (defn render
